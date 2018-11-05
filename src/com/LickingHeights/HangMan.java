310d46b2b7ee65ad94d55edf489f49404f43d378
Andrew Motley <andrew.motley.2021@student.lhschools.org>
2018-11-05 07:28:30 -0500

    worked on project in IntelliJ

--- src/com/LickingHeights/HangMan.java
+++ src/com/LickingHeights/HangMan.java
@@ -13,23 +13,29 @@ public class HangMan {
         String again;
         String player1;
         String player2;
+        String letter;
+        String[] guessbreakdown;
 
         keyboard = new Scanner(System.in);
 
             System.out.println("Welcome to the hangman game");
-            System.out.println("The rules are simple, you have unlimited guesses, but you only have 7 mistakes");
-            System.out.println("if you don't complete the secret phrase without messing up 7 times, yo mans gonna get hung");
-            System.out.println("Welcome to the hangman game");
-            System.out.println("The rules are simple, you have unlimited guesses, but you only have 7 mistakes");
+            System.out.println("The rules are simple, you have unlimited guesses, but you only have 11 mistakes");
+            System.out.println("if you don't complete the secret phrase without messing up 11 times, yo mans gonna get hung");
             System.out.println("Player 1, pick a phrase for player 2 to guess");
             secretword = keyboard.nextLine();
 
+        System.out.println("+------------------+");
+        System.out.println("|        |         |");
+        System.out.println("|        O         |");
+        System.out.println("|       /|\\        |");
+        System.out.println("|      / | \\       |");
+        System.out.println("|       / \\        |");
+        System.out.println("|      /   \\       |");
+        System.out.println("+------------------+");
+
+        //System.out.println(secretword);
 
 
-
-
-
-            again = "";
 
 
 
